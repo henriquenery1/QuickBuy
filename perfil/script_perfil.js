3b1a6c068c37f283093ac65c2d97a7c11180c64c
GiuCaroline <150097558+GiuCaroline@users.noreply.github.com>
2024-06-06 00:04:39 -0300

    feat: Adicionando página perfil incompleta

---
+++ perfil/script_perfil.js
@@ -0,0 +1,19 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const userData = JSON.parse(localStorage.getItem('user'));
+
+    if (usuarioEncontrado) {
+        const userInfoDiv = document.getElementById('user-info');
+
+        userInfoDiv.innerHTML = `
+            <h2>Cargo: ${userData.nomeUsuario}</h2>
+            <p>Cpf: ${userData.cpfUsuario}</p>
+            <p>Data de nascimento: ${userData.dataNascimento}</p>
+            <p>Data de salário: ${userData.salarioUsuario}</p>
+            <p>Data de senha: ${userData.senhaUsuario}</p>
+        `;
+
+        userInfoDiv.style.display = 'block';
+    } else {
+        console.log('Nenhum dado de usuário encontrado no localStorage.');
+    }
+});
