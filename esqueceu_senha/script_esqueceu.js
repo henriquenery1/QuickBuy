d117b8fd1b264a2b2614f584f48da8de108b6e96
Henrique Nery <88451907+henriquenery1@users.noreply.github.com>
2024-06-06 19:35:31 -0300

    feat: Adiciona página com script de esqueceu senha

--- esqueceu_senha/script_esqueceu.js
+++ esqueceu_senha/script_esqueceu.js
@@ -1,45 +1,13 @@
-const sendEmail = () => {
-    const apiKey = 'SUA_API_KEY_DO_SENDGRID';
-    const url = 'https://api.sendgrid.com/v3/mail/send';
+const enviarButton = document.querySelector('.btn_enviar')
 
-    const cpfUsuario = document.getElementById('cpfUsuario').value;
-
-    const emailData = {
-        personalizations: [
-            {
-                to: [{ email: 'giulia.claro@fatec.sp.gov.br' }],
-                subject: 'REDEFINIÇÃO DE SENHA'
-            }
-        ],
-        from: { email: 'quickbuy.app@outlook.com' },
-        content: [
-            {
-                type: 'text/plain',
-                value: `O funcionário do CPF ${cpfUsuario} está solicitando uma definição de senha.`
-            }
-        ]
-    };
-
-    fetch(url, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': `Bearer ${apiKey}`
-        },
-        body: JSON.stringify(emailData)
-    })
-    .then(response => {
-        if (response.ok) {
-            // Exibir notificação de sucesso
-            alert('E-mail enviado com sucesso!');
-            // Redirecionar para a página de login
-            window.location.href = '../login/login.html';
-        } else {
-            throw new Error('Erro ao enviar o e-mail.');
-        }
-    })
-    .catch(error => {
-        console.error('Erro:', error);
-        alert('Erro ao enviar o e-mail. Por favor, tente novamente.');
+enviarButton.addEventListener('click', function() {
+    setTimeout(function() {
+        const popup = document.getElementById('popup');
+        }, 900); 
     });
-};
+
+btnFecharPopup.addEventListener('click', function() {
+    popup.classList.remove('visible');
+    window.location.href = "../login/login.html";
+});
+
