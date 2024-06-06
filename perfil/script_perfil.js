7e1b36a63bdb98e58ad757ef9e9a33d47c015c2b
GiuCaroline <150097558+GiuCaroline@users.noreply.github.com>
2024-06-06 11:01:04 -0300

    feat: Adicionando um script global para a nav em todas as páginas

--- perfil/script_perfil.js
+++ perfil/script_perfil.js
@@ -18,31 +18,4 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 });
 
-document.addEventListener('DOMContentLoaded', function() {
-    const buttonCaixa = document.getElementById('btn_caixa');
-    const buttonProdutos = document.getElementById('btn_produtos');
-    const buttonFinanceiro = document.getElementById('btn_financeiro');
-    const buttonCadastro = document.getElementById('btn_cadastro');
-    const image = document.getElementById('navigate-image');
-
-    buttonCaixa.addEventListener('click', function() {
-        window.location.href = '../caixa/caixa.html';
-    });
-
-    buttonProdutos.addEventListener('click', function() {
-        window.location.href = '.html';
-    });
-
-    buttonFinanceiro.addEventListener('click', function() {
-        window.location.href = '../gastos/gastos.html';
-    });
-
-    buttonCadastro.addEventListener('click', function() {
-        window.location.href = '../cadastro/cadastro.html';
-    });
-
-    image.addEventListener('click', function() {
-        window.location.href = '../perfil/perfil.html';
-    });
-});
 
